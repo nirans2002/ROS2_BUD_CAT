ebea5737950ea990dbe2c59c86cca78b2d80bf5c
Niran S Narayanan <nirans2002@gmail.com>
2023-07-30 17:01:29 +0530

    angle 1

--- src/bud_cat/bud_cat/leg.py
+++ src/bud_cat/bud_cat/leg.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from sensor_msgs.msg import JointState
-
 class StatePublisher(Node):
 
     def __init__(self):
@@ -24,7 +23,8 @@ class StatePublisher(Node):
         leg_FL_angle = 0.0
         thigh_BL_angle = 0.0
         leg_BL_angle = 0.0
-
+        i= 0.01
+        
 
         # message declarations
         joint_state = JointState()
@@ -39,8 +39,11 @@ class StatePublisher(Node):
                 joint_state.name = ['thigh_FR_joint','leg_FR_joint','thigh_BR_joint','leg_BR_joint','thigh_FL_joint','leg_FL_joint','thigh_BL_joint','leg_BL_joint']
                 joint_state.position = [thigh_FR_angle,leg_FR_angle,thigh_BR_angle,leg_BR_angle,thigh_FL_angle,leg_FL_angle,thigh_BL_angle,leg_BL_angle]
                 self.joint_pub.publish(joint_state)
+                
+                if(thigh_FR_angle <= -1.57 or thigh_FR_angle >= 1.57):
+                    i = -i 
 
-                thigh_FR_angle = thigh_FR_angle +0.01
+                thigh_FR_angle = thigh_FR_angle +i    
                 # This will adjust as needed per iteration
                 loop_rate.sleep()
 
